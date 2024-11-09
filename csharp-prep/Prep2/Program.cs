f11f475f8bb45915afdc67919d80d3649cf7b747
Gee_Elemi <elemigee@gmail.com>
2024-11-09 21:42:18 +0100

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+
+        string letter ="";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is: {letter}" );
+
+         string sign = "";
+        int lastDigit = grade % 10;
+
+        if (letter == "A")
+        {
+            if (lastDigit < 3) sign = "-";  // Only A- is allowed for "A" grades
+        }
+        else if (letter == "F")
+        {
+            sign = "";  // No modifiers for F grades
+        }
+        else
+        {
+            if (lastDigit >= 7) sign = "+";
+            else if (lastDigit < 3) sign = "-";
+        }
+
+        // Display the final grade with sign
+        Console.WriteLine($"The final grade is: {letter}{sign}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Best Of luck, next time!");
+        }
     }
 }
