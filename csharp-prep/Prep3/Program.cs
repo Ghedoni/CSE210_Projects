2c13edcea7ff11b6c375dcc76364d3e356d6557f
Gee_Elemi <elemigee@gmail.com>
2024-11-09 22:53:02 +0100

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("Pick a magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = 2;
+        while (guess != magicNumber)
+        {
+            Console.Write("Can you tell us your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+        }
     }
 }
